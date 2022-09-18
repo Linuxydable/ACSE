2cc01996642da5543af0dbfa54c1f2cff785eaef
John (Linuxydable) Benard <linuxydable@linuxydable.fr>
2022-09-18 16:18:34 +0200

    Update the project to .NET 6 and .NETStandard 2.1

--- ACSE.WinForms/Program.cs
+++ ACSE.WinForms/Program.cs
@@ -15,6 +15,7 @@ namespace ACSE.WinForms
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
